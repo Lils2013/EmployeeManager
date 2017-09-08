168ccaf89d5a7b6e6fb53713779203fc0545681b
fmamedov <fariz.fmc@gmail.com>
2017-09-08 18:09:51 +0300

    added controllers to CertificatesController

--- src/main/java/ru/tsconsulting/errorHandling/not_specified_exceptions/EmployeeLastNameNotSpecifiedException.java
+++ src/main/java/ru/tsconsulting/errorHandling/not_specified_exceptions/EmployeeLastNameNotSpecifiedException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Last name not specified.")
-public class LastnameNotSpecifiedException extends AttributeNotSpecifiedException {
+public class EmployeeLastNameNotSpecifiedException extends AttributeNotSpecifiedException {
 
     public String getMessage() {
         return "Last name not specified.";
